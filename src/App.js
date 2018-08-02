0fb5c5fe1e3f759f2e30eec89687b6ebdc0f4a9e
am-mgr <am.mgr.dev@gmail.com>
2018-08-02 23:01:51 +0530

    Added material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Button, Grid } from "@material-ui/core";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>OraBoard</h1>
+        <Grid>
+          <h1>OraBoard</h1>
+          <Button variant="contained" color="secondary">
+            Ready to todo!
+          </Button>
+        </Grid>
       </div>
     );
   }
