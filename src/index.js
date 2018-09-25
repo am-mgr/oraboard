9aeb52c210dcdfb60d4d6a7b326c257ddfd6c27d
am-mgr <am.mgr.dev@gmail.com>
2018-09-25 21:26:56 +0530

    Added routing for react comps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Router, Link } from "@reach/router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => {
+  return <App />;
+};
+
+const About = () => {
+  return <p>About us</p>;
+};
+
+const AppWithRoute = () => {
+  return (
+    <div>
+      <div>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="about">About</Link>
+      </div>
+      <Router>
+        <Home path="/" />
+        <About path="about" />
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<AppWithRoute />, document.getElementById("root"));
 registerServiceWorker();
